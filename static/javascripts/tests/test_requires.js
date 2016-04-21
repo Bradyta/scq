ada6cdfc0e38dd7c0311324d99249b4ca520debb
Bradyta <brady.auen@colorado.edu>
2016-04-21 10:49:02 -0600

    updated package.json

--- static/javascripts/tests/test_requires.js
+++ static/javascripts/tests/test_requires.js
@@ -1,3 +1,9 @@
 var React = require('react'),
     assert = require('assert'),
-    TestUtils = require('react-addons-test-utils');
+    TestUtils = require('react-addons-test-utils'),
+    jsdom = require('jsdom');
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.parentWindow;
+
+
